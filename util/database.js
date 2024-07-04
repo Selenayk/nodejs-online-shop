630f058c1c9fc21e93e3bc8d3c7a97c366c7e13c
Selenayk <selenaykeskinm@gmail.com>
2024-07-04 14:04:27 +0300

    upload project files

---
+++ util/database.js
@@ -0,0 +1,39 @@
+// FOR MYSQL
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize('mydb', 'root', 'sql**sel12', {
+//   dialect: 'mysql',
+//   host: 'localhost',
+// });
+
+// module.exports = sequelize;
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://Selenay:TBzgjWL1sP7O44qQ@cluster0.apz4azt.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+  )
+    .then((client) => {
+      console.log('Connected!');
+      _db = client.db();
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
